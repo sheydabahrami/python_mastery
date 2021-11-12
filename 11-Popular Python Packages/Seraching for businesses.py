8e2bf291e3fb60e98665d6778bf73e2327d56e38
Sheyda Bahrami <seydabahrami@gmail.com>
2021-11-12 15:37:42 +0300

    gitignore added

--- 11-Popular Python Packages/Seraching for businesses.py
+++ 11-Popular Python Packages/Seraching for businesses.py
@@ -1,7 +1,6 @@
 #Searching for Businesses
 import requests
 url = 'https://api.yelp.com/v3/businesses/search'
-api_key = "b6cho1459BzmcPi3ywlneHum-7H4mKpKSlmZ3j_Pa5fYdCFKIN7V1vLJ-M4oxxhjedw4BK5oDG_b6pyj8ihwwNurgy9T4NbX9gYdz_Sahon6DL_7FsLvUGYHMhWNYXYx"
 headers = {
     "Authorization": "Bearer " + api_key
 }
